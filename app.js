777aa776ac56627859948b981ed3920e10cbf9ed
milanzagorac <zagoracm@gmail.com>
2021-02-05 20:32:21 -0500

    exercises

--- app.js
+++ app.js
@@ -18,3 +18,27 @@ function addNumbersAgain(num) {
 
 // in the function above there are a fixed number of operations, no matter the value of num. Therefore, this is constant time which is a flat line compared to the O(n) line which is 45 degrees starting from the origin (0,0).
 
+// Space Complexity
+
+function addArray(arr) {
+    let total = 0;
+    for (let i =0; i < arr.length; i++){
+        total += arr[i];
+    }
+
+    return total; 
+}
+
+//no matter the size of the array, the space is already assigned because there are only two variables and they exist no matter what. We are adding to the total variable but not making new variables, therefore this is constant space!
+
+function doubleArray(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(arr[i] * 2)
+    }
+
+    return newArr;
+}
+
+//newArr is getting longer and longer directly proportional to the input. Therefore this is O(n) space.
+
